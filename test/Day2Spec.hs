33c08eb8a81a6cd7ec11b417736daace68fb3dd5
Tobias Klug <tbs.klg@gmail.com>
2022-12-02 07:01:50 +0100

    Provide test cases

--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -1,9 +1,13 @@
 module Day2Spec where
 
-import Day2 
-import Test.Hspec (Spec, it, shouldBe)
+import Day2 (solve, solvePartTwo)
+import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
-  it "should find the maximum carriage of calories by an elf" $ do
-    
+  describe "rock paper scissors" $ do
+    it "should calculate the score when playing with a strategy guide" $ do
+      solve ["A Y", "B X", "C Z"] `shouldBe` 15
+
+    it "should calculate the score when playing with a ultra secret strategy guide" $ do
+      solvePartTwo ["A Y", "B X", "C Z"] `shouldBe` 12
