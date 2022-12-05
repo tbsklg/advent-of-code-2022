813385b8d8e2c40dbe950e397a9cb3a226484e7b
Tobias Klug <tbs.klg@gmail.com>
2022-12-05 18:55:46 +0100

    Solve day 5

--- app/Main.hs
+++ app/Main.hs
@@ -8,4 +8,4 @@ main = do
   handle <- openFile "app/resources/day5.txt" ReadMode
   contents <- hGetContents handle
   print $ solve . lines $ contents
-  -- print $ solvePartTwo . lines $ contents
+  print $ solvePartTwo . lines $ contents
--- src/Day5.hs
+++ src/Day5.hs
@@ -1,14 +1,24 @@
 module Day5 where
 
-import Data.List.Split (divvy, splitOn)
+import Data.List.Split (divvy, splitOn, splitWhen)
+import qualified Data.Map as M
 
 data Move = Move {numberOfCrates :: Int, from :: Int, to :: Int} deriving (Show, Eq)
 
-solve :: [String] -> [String]
-solve xs = xs
+solve :: [String] -> String
+solve xs = topOfEachStack . foldl performMove (crates stacks) $ moves rawMoves
+  where
+    stacks = head . splitWhen (== "") $ xs
+    rawMoves = last . splitWhen (== "") $ xs
 
-crates :: [String] -> [[String]]
-crates = group . map (divvy 1 4 . tail)
+solvePartTwo :: [String] -> String
+solvePartTwo xs = topOfEachStack . foldl performMove2 (crates stacks) $ moves rawMoves
+  where
+    stacks = head . splitWhen (== "") $ xs
+    rawMoves = last . splitWhen (== "") $ xs
+  
+crates :: [String] -> M.Map Int [String]
+crates = M.fromList . zip [1 ..] . map (filter (/= " ")) . group . map (divvy 1 4 . tail)
   where
     group [] = []
     group xs
@@ -25,3 +35,28 @@ move xs =
       from = read . (!! 3) . splitOn " " $ xs,
       to = read . (!! 5) . splitOn " " $ xs
     }
+
+performMove :: M.Map Int [String] -> Move -> M.Map Int [String]
+performMove ship Move {numberOfCrates = n, from = f, to = t} = targetShip
+  where
+    (sourceCrate, sourceShip) = case M.lookup f ship of
+      Just a -> (take n a, M.insert f (drop n a) ship)
+      Nothing -> ([], ship)
+
+    targetShip = case M.lookup t sourceShip of
+      Just a -> M.insert t (reverse sourceCrate ++ a) sourceShip
+      Nothing -> ship
+
+performMove2 :: M.Map Int [String] -> Move -> M.Map Int [String]
+performMove2 ship Move {numberOfCrates = n, from = f, to = t} = targetShip
+  where
+    (sourceCrate, sourceShip) = case M.lookup f ship of
+      Just a -> (take n a, M.insert f (drop n a) ship)
+      Nothing -> ([], ship)
+
+    targetShip = case M.lookup t sourceShip of
+      Just a -> M.insert t (sourceCrate ++ a) sourceShip
+      Nothing -> ship
+
+topOfEachStack :: M.Map a [String] -> String
+topOfEachStack = concatMap (head . snd) . M.toList
