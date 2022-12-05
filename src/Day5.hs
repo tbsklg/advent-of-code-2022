a78ce1a98521ae159b598c7aaea31437b9b5123a
Tobias Klug <tbs.klg@gmail.com>
2022-12-05 10:49:13 +0100

    WIP

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,13 +1,27 @@
 module Day5 where
-import Data.List.Split (splitOn, divvy)
+
+import Data.List.Split (divvy, splitOn)
+
+data Move = Move {numberOfCrates :: Int, from :: Int, to :: Int} deriving (Show, Eq)
 
 solve :: [String] -> [String]
 solve xs = xs
 
 crates :: [String] -> [[String]]
 crates = group . map (divvy 1 4 . tail)
-    where
-        group [] = []
-        group xs
-            | all null xs = []
-            | otherwise  = map head xs : (group . map tail $ xs)
+  where
+    group [] = []
+    group xs
+      | all null xs = []
+      | otherwise = map head xs : (group . map tail $ xs)
+
+moves :: [String] -> [Move]
+moves = map move
+
+move :: String -> Move
+move xs =
+  Move
+    { numberOfCrates = read . (!! 1) . splitOn " " $ xs,
+      from = read . (!! 3) . splitOn " " $ xs,
+      to = read . (!! 5) . splitOn " " $ xs
+    }
--- test/Day5Spec.hs
+++ test/Day5Spec.hs
@@ -1,10 +1,20 @@
 module Day5Spec where
 
 import Day5
+import Day5 (Move (numberOfCrates))
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
   describe "supply stacks" $ do
-    it "should load load the ship" $ do
-        crates ["    [D]    ","[N] [C]    ","[Z] [M] [P]"] `shouldBe` [[" ", "N", "Z"], ["D","C","M"], [" "," ","P"]]
+    it "should convert crates from raw data" $ do
+      crates ["    [D]    ", "[N] [C]    ", "[Z] [M] [P]"] `shouldBe` [[" ", "N", "Z"], ["D", "C", "M"], [" ", " ", "P"]]
+
+    it "should convert moves from raw data" $ do
+      moves ["move 1 from 2 to 1", "move 3 from 1 to 3", "move 2 from 2 to 1", "move 1 from 1 to 2"]
+        `shouldBe` [
+            (Move {numberOfCrates = 1, from = 2, to = 1}),
+            (Move {numberOfCrates = 3, from = 1, to = 3}),
+            (Move {numberOfCrates = 2, from = 2, to = 1}),
+            (Move {numberOfCrates = 1, from = 1, to = 2})
+        ]
