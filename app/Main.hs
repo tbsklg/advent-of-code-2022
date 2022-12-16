e464145685cb1d62a2bb457444c6f50c8a8bef89
Tobias Klug <tbs.klg@gmail.com>
2022-12-16 13:24:10 +0100

    Solve day 14

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,10 @@ module Main where
 
 import System.IO (IOMode (ReadMode), hGetContents, openFile)
 import Day14
-import Data.List (intercalate, maximumBy)
   
 main :: IO ()
 main = do
   handle <- openFile "app/resources/day14.txt" ReadMode
   contents <- hGetContents handle
   print $ solve $ lines contents
-  -- putStrLn $ intercalate "\n" . solvePartTwo $ lines contents
   print $ solvePartTwo $ lines contents
--- src/Day14.hs
+++ src/Day14.hs
@@ -3,6 +3,7 @@ module Day14 where
 import Data.Function (on)
 import Data.List (maximumBy, minimumBy, nub)
 import Data.List.Split (splitOn)
+import qualified Data.Set as S
 
 type Point = (Int, Int)
 
@@ -11,19 +12,10 @@ type Grid = [String]
 data Path = Path {from :: Point, to :: Point} deriving (Show, Eq)
 
 solve :: [String] -> Int
-solve = length . filter (=='o') . concat . simulateFalling . grid (500,0) . parse
+solve = simulateFalling . grid (500, 0) . parse
 
 solvePartTwo :: [String] -> Int
-solvePartTwo xs = length . filter (=='o') . concat . simulateFalling2 . grid (500,0) $ extendedPoints
-  where
-    extendedPoints = flatten (minX - 2, maxY + 2) (maxX + 2, maxY + 2) ++ points
-
-    maxX = maximum . map fst $ points
-    minX = minimum . map fst $ points
-    maxY = maximum . map snd $ points
-    minY = minimum . map snd $ points
-
-    points = parse xs
+solvePartTwo xs = simulateFalling2 . grid (500, 0) $ parse xs
 
 parse :: [String] -> [Point]
 parse [] = []
@@ -38,79 +30,60 @@ extractPoint xs = (read . head $ parts, read . last $ parts)
   where
     parts = splitOn "," xs
 
-normalize :: [Point] -> Point -> ([Point], Point)
-normalize points (sx, sy) = (normalizedPoints, normalizedPouringPosition)
-  where
-    normalizedPoints = map normalized points
-    normalizedPouringPosition = (sx - minX, sy)
-
-    normalized (x, y) = (x - minX, y)
-
-    minX = minimum . map fst $ points
-    minY = minimum . map snd $ points
-
 air :: Char
 air = '.'
 
 sand :: (Int, Int)
 sand = (500, 0)
 
-grid :: Point -> [Point] -> (Point, Grid)
-grid pouringPosition paths = (normalizedPouringPosition, buildRocks normalizedPoints initialGrid)
+grid :: Point -> [Point] -> (Point, S.Set Point)
+grid pouringPosition points = (pouringPosition, buildRocks points initialGrid)
   where
     buildRocks [] grid = grid
-    buildRocks (p : ps) grid = buildRocks ps (replace p '#' grid)
+    buildRocks (p : ps) grid = buildRocks ps (S.insert p grid)
 
-    (normalizedPoints, normalizedPouringPosition) = normalize paths pouringPosition
-    initialGrid = emptyGridFrom normalizedPoints
+    initialGrid = S.empty
 
-simulateFalling :: (Point, Grid) -> Grid
+simulateFalling :: (Point, S.Set Point) -> Int
 simulateFalling (pouringPosition, abc) = fallDownFrom pouringPosition abc
   where
-    fallDownFrom (x, y) grid = case fallDown (x,y) grid of
-      Just a -> fallDownFrom pouringPosition (replace a 'o' grid)
-      Nothing -> grid
+    maxY = maximum . map snd . S.toList $ abc
 
-simulateFalling2 :: (Point, Grid) -> Grid
+    fallDownFrom (x, y) grid = case fallDown (x, y) maxY grid of
+      Just a -> 1 + fallDownFrom pouringPosition (S.insert a grid)
+      Nothing -> 0
+
+simulateFalling2 :: (Point, S.Set Point) -> Int
 simulateFalling2 (pouringPosition, abc) = fallDownFrom pouringPosition abc
   where
-    fallDownFrom (x, y) grid = case fallDown (x,y) grid of
-      Just a
-        | a == pouringPosition -> replace pouringPosition 'o' grid
-        | otherwise -> fallDownFrom pouringPosition (replace a 'o' grid)
-      Nothing -> simulateFalling2 ((fst pouringPosition + 1, snd pouringPosition), expandGrid grid)
-  
-fallDown :: Point -> Grid -> Maybe Point
-fallDown _ [] = Nothing
-fallDown point@(x, y) grid
-  | outOfBounds point grid = Nothing
+    maxY = (+) 1 . maximum . map snd . S.toList $ abc
+
+    fallDownFrom (x, y) grid = case fallDown2 (x, y) maxY grid of
+      Just a -> 1 + fallDownFrom pouringPosition (S.insert a grid)
+      Nothing -> 0
+
+fallDown :: Point -> Int -> S.Set Point -> Maybe Point
+fallDown point@(x, y) maxY grid
   | blocked point grid = Nothing
-  | not . blocked (x, y + 1) $ grid = fallDown (x, y + 1) grid
-  | not . blocked (x - 1, y + 1) $ grid = fallDown (x - 1, y + 1) grid
-  | not . blocked (x + 1, y + 1) $ grid = fallDown (x + 1, y + 1) grid
+  | y == maxY = Nothing
+  | not . blocked2 (x, y + 1) maxY $ grid = fallDown (x, y + 1) maxY grid
+  | not . blocked2 (x - 1, y + 1) maxY $ grid = fallDown (x - 1, y + 1) maxY grid
+  | not . blocked2 (x + 1, y + 1) maxY $ grid = fallDown (x + 1, y + 1) maxY grid
   | otherwise = Just (x, y)
 
-maxY :: Grid -> Int
-maxY = length
+fallDown2 :: Point -> Int -> S.Set Point -> Maybe Point
+fallDown2 point@(x, y) maxY grid
+  | blocked2 point maxY grid = Nothing
+  | not . blocked2 (x, y + 1) maxY $ grid = fallDown2 (x, y + 1) maxY grid
+  | not . blocked2 (x - 1, y + 1) maxY $ grid = fallDown2 (x - 1, y + 1) maxY grid
+  | not . blocked2 (x + 1, y + 1) maxY $ grid = fallDown2 (x + 1, y + 1) maxY grid
+  | otherwise = Just (x, y)
 
-expandGrid :: Grid -> Grid
-expandGrid [] = []
-expandGrid [r] = ['#' : r ++ ['#']]
-expandGrid (r:rs) = ('.' : r ++ ['.']) : expandGrid rs
+blocked2 :: Point -> Int -> S.Set Point -> Bool
+blocked2 point@(x,y) maxY s = S.member point s || y > maxY
 
-blocked :: Point -> Grid -> Bool
-blocked (x, y) grid = (grid !! y !! x) `elem` "#o"
-
-outOfBounds :: Point -> Grid -> Bool
-outOfBounds _ [] = True
-outOfBounds (x, y) grid@(g : gs) = x == 0 || x > (length g - 2)
-
-replace :: Point -> Char -> Grid -> Grid
-replace (x, y) toInsert grid = nextGrid
-  where
-    lineToUpdate = (!! max 0 y) grid
-    updatedLine = take x lineToUpdate ++ [toInsert] ++ drop (x + 1) lineToUpdate
-    nextGrid = take y grid ++ [updatedLine] ++ drop (y + 1) grid
+blocked :: Point -> S.Set Point -> Bool
+blocked = S.member
 
 flatten :: Point -> Point -> [Point]
 flatten from = nub . go from
@@ -118,9 +91,3 @@ flatten from = nub . go from
     go (x, y) (x', y')
       | x == x' = map (\z -> (x, z)) [minimum [y, y'] .. maximum [y, y']]
       | otherwise = map (\z -> (z, y)) [minimum [x, x'] .. maximum [x, x']]
-
-emptyGridFrom :: [Point] -> Grid
-emptyGridFrom points = foldl (\y x -> y ++ [replicate xSize air]) [] [0 .. ySize - 1]
-  where
-    xSize = (+ 1) . maximum . map fst $ points
-    ySize = (+ 1) . maximum . map snd $ points
