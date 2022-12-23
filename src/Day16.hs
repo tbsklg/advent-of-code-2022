cd9bebce4ffac9c841ea1430c2c04faacd56fcf3
Tobias Klug <tbs.klg@gmail.com>
2022-12-23 15:51:35 +0100

    Provide min distance

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,17 +1,49 @@
 module Day16 where
 
+import Data.List (nub)
+import qualified Data.Map as M
+import Data.Maybe (fromJust)
+import qualified Data.Set as S
 import qualified Text.Parsec as P
 import Text.Parsec.Char (letter)
 import Text.Parsec.Combinator (sepBy)
 
 data Valve = Valve {name :: String, flowRate :: Int, neighbours :: [String]} deriving (Show, Eq)
 
+type Tunnel = M.Map String Valve
+
+data DFS = DFS {queue :: [String], opened :: [String], minute :: Int, pressure :: Int} deriving (Show, Eq)
+
 solve :: IO String -> IO ()
 solve xs = do
   result <- parseValves "" <$> xs
   case result of
     Left a -> error "Input parsing not working!"
-    Right a -> putStrLn ("Parse OK!" ++ show a)
+    Right a -> print a
+
+minDistance :: String -> String -> M.Map String Valve -> Maybe Int
+minDistance start end tunnel = go (S.singleton start) 0 (S.singleton start)
+  where
+    go toVisit minute visited
+      | S.null toVisit = Nothing
+      | S.member end toVisit = Just minute
+      | otherwise = go newToVisit (minute + 1) newVisited
+      where
+        newToVisit =
+          S.difference
+            ( S.unions
+                ( map
+                    ( \x ->
+                        S.fromList (neighbours (tunnel M.! x))
+                    )
+                    (S.toList toVisit)
+                )
+            )
+            visited
+        newVisited = S.union visited newToVisit
+
+valvesWithFlowGreaterThanZero :: Tunnel -> [String]
+valvesWithFlowGreaterThanZero tunnel = filter (\x -> flowRate (tunnel M.! x) > 0) (M.keys tunnel)
 
 parseInt :: P.Parsec String () Int
 parseInt = (negate <$> (P.char '-' *> (read <$> P.many1 P.digit))) P.<|> (read <$> P.many1 P.digit)
--- test/Day16Spec.hs
+++ test/Day16Spec.hs
@@ -1,13 +1,36 @@
 module Day16Spec where
 
+import qualified Data.Map as M
+import qualified Data.Set as S
 import Day16
 import Test.Hspec (Spec, describe, it, shouldBe)
 import qualified Text.Parsec as P
 
 testValves = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\nValve HH has flow rate=22; tunnel leads to valve GG\n"
 
+tunnel :: M.Map [Char] Valve
+tunnel =
+  M.fromList
+    [ ("AA", Valve {name = "AA", flowRate = 0, neighbours = ["DD", "II", "BB"]}),
+      ("BB", Valve {name = "BB", flowRate = 13, neighbours = ["CC", "AA"]}),
+      ("CC", Valve {name = "CC", flowRate = 2, neighbours = ["DD", "BB"]}),
+      ("DD", Valve {name = "DD", flowRate = 20, neighbours = ["CC", "AA", "EE"]}),
+      ("EE", Valve {name = "EE", flowRate = 3, neighbours = ["FF", "DD"]}),
+      ("FF", Valve {name = "FF", flowRate = 0, neighbours = ["EE", "GG"]}),
+      ("GG", Valve {name = "GG", flowRate = 0, neighbours = ["FF", "HH"]}),
+      ("HH", Valve {name = "HH", flowRate = 22, neighbours = ["GG"]}),
+      ("II", Valve {name = "II", flowRate = 0, neighbours = ["AA", "JJ"]}),
+      ("JJ", Valve {name = "JJ", flowRate = 21, neighbours = ["II"]})
+    ]
+
 spec :: Spec
 spec = do
   describe "Proboscidea Volcanium" $ do
     it "should parse valves" $ do
       parseValves "" testValves `shouldBe` Right [Valve {name = "AA", flowRate = 0, neighbours = ["DD", "II", "BB"]}, Valve {name = "HH", flowRate = 22, neighbours = ["GG"]}]
+
+    it "should find valves with flow rate greater than zero" $ do
+      valvesWithFlowGreaterThanZero tunnel `shouldBe` ["BB", "CC", "DD", "EE", "HH", "JJ"]
+
+    it "should walk within the tunnel" $ do
+      minDistance "AA" "DD" tunnel `shouldBe` Just 1
